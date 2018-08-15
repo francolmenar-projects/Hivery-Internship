d7319864ed254e50184e9c5697778534ee9867d2
francolmenar <100346094@alumnos.uc3m.es>
2018-08-15 04:06:09 +0200

    First part done

--- js/game.js
+++ js/game.js
@@ -15,30 +15,39 @@ function createTimer(){
     // Set the default countdown time
     var time_left = countdown;
     var min, second, new_time;
+    // This code is executed each second
     setInterval(function () {
         new_time = "";
         second = time_left % 60;
         min = parseInt(time_left / 60);
+        // Check if there is any digit in the minutes
         if(min > 0){
+            // Check how many digits the minutes has
             if(min >= 10){
                 new_time = min.toString() + ":";
             }
+            // If the minutes has only one digit we add a "0" to the left side of it
             else{
                 new_time = "0" + min.toString() + ":";
             }
         }
+        // There are no minutes left, so we add another "0" to the minutes
         else{
             new_time = min.toString() + "0:";
         }
+        // Check if there are two digits in the seconds
         if(second >= 10){
             new_time += second.toString();
         }
+        // There is only one digit in the second side, so we add a "0" to the left side
         else{
             new_time += "0" + second.toString();
         }
-        console.log(new_time);
+        // Get the HTML of the time and assign to it the new value of the timer
         document.getElementById("timer").innerHTML = new_time;
+        // Update the timer value for the next iteration
         time_left--;
+        // If there is no time left the time is over
         if(time_left < 0){
             goToGameOver();
         }
@@ -58,4 +67,5 @@ function setActualProfit() {
     // Set the profit
     setActualProfit();
     // Setting the listeners
+    document.getElementById("moneyCounterDiv").addEventListener("click", goToGameOver); // ONLY FOR TESTING
     }());
